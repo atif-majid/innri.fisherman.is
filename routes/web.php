3a39ddba4ca40c3067c30bc7cf48012f52fae84c
atif-majid <atif.majid10@gmail.com>
2021-02-24 17:32:58 +0500

    Redirect to login page if user clicked a link in email and not logged in to app already

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,11 @@ Route::get('/home', ['middleware' => 'auth', App\Http\Controllers\HomeController
 Route::get('/change-password', ['middleware' => 'auth', App\Http\Controllers\ChangePasswordController::class, 'index'])->name('change-password');
 Route::post('/change-password', ['middleware' => 'auth', App\Http\Controllers\ChangePasswordController::class, 'store'])->name('change.password');
 
-Route::get('employees/outstandingitems', ['as'=>'employees.outstandingitems', 'uses'=>'App\Http\Controllers\EmployeesController@outstandingitems']);
+Route::get('employees/outstandingitems', ['as'=>'employees.outstandingitems', 'uses'=>'App\Http\Controllers\EmployeesController@outstandingitems'])->middleware('auth');;
 Route::resource('employees', EmployeesController::class, ['middleware' => 'auth']);
 Route::post('empajax/request/status', [EmployeesController::class, 'empajaxRequestStore'])->name('empajax.request.status');
 Route::post('empajax/request/picture', [EmployeesController::class, 'empajaxRequestPicture'])->name('empajax.request.picture');
-Route::get('employees/onboarding/{id}', ['as'=>'employees.onboarding', 'uses'=>'App\Http\Controllers\EmployeesController@onboarding']);
+Route::get('employees/onboarding/{id}', ['as'=>'employees.onboarding', 'uses'=>'App\Http\Controllers\EmployeesController@onboarding'])->middleware('auth');;
 Route::post('/employees/storeonboarding', ['as'=>'employees.storeonboarding', 'uses'=>'App\Http\Controllers\EmployeesController@storeonboarding']);
 Route::post('/employees/updateonboardstatus', ['as'=>'employees.updateonboardstatus', 'uses'=>'App\Http\Controllers\EmployeesController@updateonboardstatus']);
 
@@ -48,7 +48,7 @@ Route::resource('recipes', RecipesController::class, ['middleware' => 'auth']);
 Route::get('recipes/getpdf/{id}', ['as'=>'recipes.getpdf', 'uses'=>'App\Http\Controllers\RecipesController@getpdf']);
 
 Route::resource('improvements', ImprovementsController::class, ['middleware' => 'auth']);
-Route::get('improvements/process/{id}', ['as'=>'improvements.process', 'uses'=>'App\Http\Controllers\ImprovementsController@process']);
+Route::get('improvements/process/{id}', ['as'=>'improvements.process', 'uses'=>'App\Http\Controllers\ImprovementsController@process'])->middleware('auth');;
 Route::post('/improvements/updateprocess', ['as'=>'improvements.updateprocess', 'uses'=>'App\Http\Controllers\ImprovementsController@updateprocess']);
 Route::post('/improvements/updatecomment', ['as'=>'improvements.updatecomment', 'uses'=>'App\Http\Controllers\ImprovementsController@updatecomment']);
 Route::post('/improvements/updateimpstatus', ['as'=>'improvements.updateimpstatus', 'uses'=>'App\Http\Controllers\ImprovementsController@updateimpstatus']);
@@ -56,13 +56,14 @@ Route::post('/improvements/uploadpicture', ['as'=>'improvements.uploadpicture', 
 
 Route::post('sitesettings/storeonboardingcategories', ['as'=>'sitesettings.storeonboardingcategories', 'uses'=>'App\Http\Controllers\SitesettingsController@storeonboardingcategories']);
 Route::post('sitesettings/storeonboardingtasks', ['as'=>'sitesettings.storeonboardingtasks', 'uses'=>'App\Http\Controllers\SitesettingsController@storeonboardingtasks']);
-Route::get('sitesettings/onboardingsections/', [SitesettingsController::class, 'onboardingsections'])->name('sitesettings.onboardingsections');
-Route::get('sitesettings/onboardingtasks', ['as'=>'sitesettings.onboardingtasks', 'uses'=>'App\Http\Controllers\SitesettingsController@onboardingtasks']);
+Route::get('sitesettings/onboardingsections/', [SitesettingsController::class, 'onboardingsections'])->name('sitesettings.onboardingsections')->middleware('auth');;
+Route::get('sitesettings/onboardingtasks', ['as'=>'sitesettings.onboardingtasks', 'uses'=>'App\Http\Controllers\SitesettingsController@onboardingtasks'])->middleware('auth');;
 Route::resource('sitesettings', SitesettingsController::class, ['middleware' => 'auth']);
 
 Route::resource('salesopportunity', SalesopportunityController::class, ['middleware' => 'auth']);
 Route::post('/salesopportunity/uploadpicture', ['as'=>'salesopportunity.uploadpicture', 'uses'=>'App\Http\Controllers\SalesopportunityController@uploadpicture']);
 Route::post('/salesopportunity/updatepost', ['as'=>'salesopportunity.updatepost', 'uses'=>'App\Http\Controllers\SalesopportunityController@updatepost']);
-Route::get('salesopportunity/process/{id}', ['as'=>'salesopportunity.process', 'uses'=>'App\Http\Controllers\SalesopportunityController@process']);
+Route::get('salesopportunity/process/{id}', ['as'=>'salesopportunity.process', 'uses'=>'App\Http\Controllers\SalesopportunityController@process'])->middleware('auth');
 Route::post('/salesopportunity/updateprocess', ['as'=>'salesopportunity.updateprocess', 'uses'=>'App\Http\Controllers\SalesopportunityController@updateprocess']);
 Route::post('/salesopportunity/updatecomment', ['as'=>'salesopportunity.updatecomment', 'uses'=>'App\Http\Controllers\SalesopportunityController@updatecomment']);
+Route::post('/salesopportunity/updatesopstatus', ['as'=>'salesopportunity.updatesopstatus', 'uses'=>'App\Http\Controllers\SalesopportunityController@updatesopstatus']);
