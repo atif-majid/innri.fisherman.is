54b17bf3e9b1a21cda2e22de9beea0b219c219fc
atif-majid <atif.majid10@gmail.com>
2021-03-19 18:25:00 +0500

    Diplay active tasks, improvements and sales opportunites assigned to current user under bell icon in header. Also adjust the count display on bell icon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Improvements;
+use App\Models\Salesopportunity;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +28,34 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        view()->composer('*', function ($view)
+        {
+            if(!Auth::guest())
+            {
+                $nCurrUserID = Auth::user()->getempid();
+                $tasks = DB::table('onboardingtasks')
+                    ->leftJoin('employees', 'employee', '=', 'employees.id')
+                    ->leftJoin('sitesettings', 'onboardingtasks.task', '=', 'sitesettings.value')
+                    ->select('onboardingtasks.*','employees.name', 'sitesettings.field')
+                    ->where('responsible_person', $nCurrUserID)
+                    ->whereIn('onboardingtasks.status', array('Not Started', 'In Progress'))
+                    ->get();
+                $Improvements = Improvements::
+                where('completed', 'no')
+                    ->where('assigned_to', $nCurrUserID)
+                    ->get();
+
+                $Salesopportunities = Salesopportunity::
+                where('status', 'In Progress')
+                    ->where('assigned_to', $nCurrUserID)
+                    ->get();
+                $nSumAll = count($tasks) + count($Improvements) + count($Salesopportunities);
+
+                $view->with('header_tasks', $tasks )
+                    ->with('header_Improvements', $Improvements)
+                    ->with('header_Salesopportunities', $Salesopportunities)
+                    ->with('header_sumall', $nSumAll);
+            }
+        });
     }
 }
--- resources/views/headericons.blade.php
+++ resources/views/headericons.blade.php
@@ -36,118 +36,99 @@
                             <ul class="search-list"></ul>
                         </div>
                     </li>
-                    <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i class="ficon bx bx-bell bx-tada bx-flip-horizontal"></i><span class="badge badge-pill badge-danger badge-up">5</span></a>
+                    <li class="dropdown dropdown-notification nav-item"><a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i class="ficon bx bx-bell bx-tada bx-flip-horizontal"></i><span class="badge badge-pill badge-danger badge-up">{{$header_sumall}}</span></a>
                         <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
+                            @php
+                                $strDispMsg = "";
+                                if(count($header_Improvements)>0)
+                                {
+                                    $strDispMsg = count($header_Improvements)." improvements";
+                                }
+                                if(count($header_Salesopportunities)>0)
+                                {
+                                    if($strDispMsg!="")
+                                    {
+                                        $strDispMsg = $strDispMsg.", ";
+                                    }
+                                    $strDispMsg = $strDispMsg.count($header_Salesopportunities)." improvements";
+                                }
+                                if(count($header_tasks)>0)
+                                {
+                                    if($strDispMsg!="")
+                                    {
+                                        $strDispMsg = $strDispMsg.", ";
+                                    }
+                                    $strDispMsg = $strDispMsg.count($header_tasks)." on/off boarding tasks";
+                                }
+                            @endphp
                             <li class="dropdown-menu-header">
-                                <div class="dropdown-header px-1 py-75 d-flex justify-content-between"><span class="notification-title">7 new Notification</span><span class="text-bold-400 cursor-pointer">Mark all as read</span></div>
+                                <div class="dropdown-header px-1 py-75 d-flex justify-content-between" style="background-color: #525253 !important;"><span class="notification-title">{{$strDispMsg}}</span><!--<span class="text-bold-400 cursor-pointer">Mark all as read</span>--></div>
                             </li>
-                            <li class="scrollable-container media-list"><a class="d-flex justify-content-between" href="javascript:void(0)">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar mr-1 m-0"><img src="app-assets/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="39" width="39"></div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">Congratulate Socrates Itumay</span> for work anniversaries</h6><small class="notification-text">Mar 15 12:32pm</small>
-                                        </div>
-                                    </div>
-                                </a>
-                                <div class="d-flex justify-content-between read-notification cursor-pointer">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar mr-1 m-0"><img src="app-assets/images/portrait/small/avatar-s-16.jpg" alt="avatar" height="39" width="39"></div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">New Message</span> received</h6><small class="notification-text">You have 18 unread messages</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between cursor-pointer">
-                                    <div class="media d-flex align-items-center py-0">
-                                        <div class="media-left pr-0"><img class="mr-1" src="app-assets/images/icon/sketch-mac-icon.png" alt="avatar" height="39" width="39"></div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">Updates Available</span></h6><small class="notification-text">Sketch 50.2 is currently newly added</small>
-                                        </div>
-                                        <div class="media-right pl-0">
-                                            <div class="row border-left text-center">
-                                                <div class="col-12 px-50 py-75 border-bottom">
-                                                    <h6 class="media-heading text-bold-500 mb-0">Update</h6>
+                            <li class="scrollable-container media-list">
+                                @if(count($header_Improvements)>0)
+                                    @foreach($header_Improvements as $improvement)
+                                        <a class="d-flex justify-content-between" href="{{ route('improvements.process', $improvement->id) }}">
+                                            <div class="media d-flex align-items-center">
+                                                <div class="media-left pr-0">
+                                                    <div class="avatar bg-primary bg-lighten-5 mr-1 m-0 p-25"><span class="avatar-content text-primary font-medium-2">IMP</span></div>
                                                 </div>
-                                                <div class="col-12 px-50 py-75">
-                                                    <h6 class="media-heading mb-0">Close</h6>
+                                                <div class="media-body">
+                                                    <h6 class="media-heading">
+                                                        <span class="text-bold-500">
+                                                            @if(strlen($improvement->description)>50)
+                                                                {{substr($improvement->description, 0, 50)}}...
+                                                            @else
+                                                                {{$improvement->description}}
+                                                            @endif
+                                                        </span>
+                                                    </h6><small class="notification-text">{{$improvement->due_date}}</small>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between cursor-pointer">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar bg-primary bg-lighten-5 mr-1 m-0 p-25"><span class="avatar-content text-primary font-medium-2">LD</span></div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">New customer</span> is registered</h6><small class="notification-text">1 hrs ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cursor-pointer">
-                                    <div class="media d-flex align-items-center justify-content-between">
-                                        <div class="media-left pr-0">
-                                            <div class="media-body">
-                                                <h6 class="media-heading">New Offers</h6>
+                                        </a>
+                                    @endforeach
+                                @endif
+                                @if(count($header_Salesopportunities)>0)
+                                    @foreach($header_Salesopportunities as $Salesopportunity)
+                                        <a class="d-flex justify-content-between" href="{{ route('salesopportunity.process', $Salesopportunity->id) }}">
+                                            <div class="media d-flex align-items-center">
+                                                <div class="media-left pr-0">
+                                                    <div class="avatar bg-primary bg-lighten-5 mr-1 m-0 p-25"><span class="avatar-content text-primary font-medium-2">SALE</span></div>
+                                                </div>
+                                                <div class="media-body">
+                                                    <h6 class="media-heading">
+                                                    <span class="text-bold-500">
+                                                        @if(strlen($Salesopportunity->description)>50)
+                                                            {{substr($Salesopportunity->description, 0, 50)}}...
+                                                        @else
+                                                            {{$Salesopportunity->description}}
+                                                        @endif
+                                                    </span>
+                                                    </h6>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="media-right">
-                                            <div class="custom-control custom-switch">
-                                                <input class="custom-control-input" type="checkbox" checked id="notificationSwtich">
-                                                <label class="custom-control-label" for="notificationSwtich"></label>
+                                        </a>
+                                    @endforeach
+                                @endif
+                                @if(count($header_tasks)>0)
+                                    @foreach($header_tasks as $task)
+                                        <a class="d-flex justify-content-between" href="#">
+                                            <div class="media d-flex align-items-center">
+                                                <div class="media-left pr-0">
+                                                    <div class="avatar bg-primary bg-lighten-5 mr-1 m-0 p-25"><span class="avatar-content text-primary font-medium-2">TASK</span></div>
+                                                </div>
+                                                <div class="media-body">
+                                                    <h6 class="media-heading">
+                                                <span class="text-bold-500">
+                                                    {{$task->task}}
+                                                </span>
+                                                    </h6><small class="notification-text">{{$task->due_date}}</small>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between cursor-pointer">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar bg-danger bg-lighten-5 mr-1 m-0 p-25"><span class="avatar-content"><i class="bx bxs-heart text-danger"></i></span></div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">Application</span> has been approved</h6><small class="notification-text">6 hrs ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between read-notification cursor-pointer">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar mr-1 m-0"><img src="app-assets/images/portrait/small/avatar-s-4.jpg" alt="avatar" height="39" width="39"></div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">New file</span> has been uploaded</h6><small class="notification-text">4 hrs ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between cursor-pointer">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar bg-rgba-danger m-0 mr-1 p-25">
-                                                <div class="avatar-content"><i class="bx bx-detail text-danger"></i></div>
-                                            </div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">Finance report</span> has been generated</h6><small class="notification-text">25 hrs ago</small>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-between cursor-pointer">
-                                    <div class="media d-flex align-items-center border-0">
-                                        <div class="media-left pr-0">
-                                            <div class="avatar mr-1 m-0"><img src="app-assets/images/portrait/small/avatar-s-16.jpg" alt="avatar" height="39" width="39"></div>
-                                        </div>
-                                        <div class="media-body">
-                                            <h6 class="media-heading"><span class="text-bold-500">New customer</span> comment recieved</h6><small class="notification-text">2 days ago</small>
-                                        </div>
-                                    </div>
-                                </div>
+                                        </a>
+                                    @endforeach
+                                @endif
                             </li>
-                            <li class="dropdown-menu-footer"><a class="dropdown-item p-50 text-primary justify-content-center" href="javascript:void(0)">Read all notifications</a></li>
+                            <!--<li class="dropdown-menu-footer"><a class="dropdown-item p-50 text-primary justify-content-center" href="javascript:void(0)">Read all notifications</a></li>-->
                         </ul>
                     </li>
                     <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
